2ca81ce604e5206d43ef900d34d6f85b4616fd06
Khalid-Ace7 <kuddin@flemingcollege.ca>
2024-01-15 13:43:59 -0500

    modified both files to clone
    idk what was said

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print lines from zen of python
 print("Sparse is better than dense")
-print("Special cases aren't special enough to break the rules")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # print lines from the zen of python
 print("Beautiful is better than ugly")
 print("Complex is better than complicated")
+print("Special cases aren't special enough to break the rules")
