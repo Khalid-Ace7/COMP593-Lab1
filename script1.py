11c3f3b359e6ebee3e3f355aec1984f7bb426876
Khalid-Ace7 <kuddin@flemingcollege.ca>
2024-01-15 13:24:19 -0500

    Revert "modified both script files"
    This reverts commit 162388dd3ce35923791071ce545a342e656ae8f2.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 # print lines from zen of python
 print("Sparse is better than dense")
+print("Special cases aren't special enough to break the rules")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-# print lines from the zen of python
 print("Beautiful is better than ugly")
 print("Complex is better than complicated")
